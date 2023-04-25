be0dc30372a8676e9dbed7a72eac31c1c0e224d0
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-26 04:59:26 +0900

    Rename mysql git repository

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ git clone git@github.com:ryusukefujisaki/mypage-frontend-app.git frontend-app
 git clone git@github.com:ryusukefujisaki/mypage-frontend-nginx.git frontend-nginx
 git clone git@github.com:ryusukefujisaki/mypage-backend-app.git backend-app
 git clone git@github.com:ryusukefujisaki/mypage-backend-nginx.git backend-nginx
-git clone git@github.com:ryusukefujisaki/mypage-mysql.git mysql
+git clone git@github.com:ryusukefujisaki/mysql.git mysql
