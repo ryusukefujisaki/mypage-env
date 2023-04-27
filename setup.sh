aa28fc55228290f8d172e03e56d8c620b3b1950e
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-27 12:26:24 +0900

    Update nginx repository

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -eux
 
 git clone git@github.com:ryusukefujisaki/mypage-frontend-app.git frontend-app
-git clone git@github.com:ryusukefujisaki/mypage-frontend-nginx.git frontend-nginx
+git clone git@github.com:ryusukefujisaki/nginx.git frontend-nginx
 git clone git@github.com:ryusukefujisaki/mypage-backend-app.git backend-app
-git clone git@github.com:ryusukefujisaki/mypage-backend-nginx.git backend-nginx
+git clone -b mypage_backend git@github.com:ryusukefujisaki/nginx.git backend-nginx
 git clone git@github.com:ryusukefujisaki/mysql.git mysql
