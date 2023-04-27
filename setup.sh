874732171cc26be588fc5d114c35eeac3a026111
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-27 13:04:37 +0900

    Rename app repositories

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-git clone git@github.com:ryusukefujisaki/mypage-frontend-app.git frontend-app
+git clone git@github.com:ryusukefujisaki/mypage-frontend.git frontend-app
 git clone git@github.com:ryusukefujisaki/nginx.git frontend-nginx
-git clone git@github.com:ryusukefujisaki/mypage-backend-app.git backend-app
+git clone git@github.com:ryusukefujisaki/mypage-backend.git backend-app
 git clone -b mypage_backend git@github.com:ryusukefujisaki/nginx.git backend-nginx
 git clone git@github.com:ryusukefujisaki/mysql.git mysql
